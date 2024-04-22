d36fc702b556a79299131d3d17567319076deddd
Jean-Michel Dusserre <jedusser@z1r9p1.42lyon.fr>
2024-04-22 11:23:16 +0200

    dd

--- s_sources/server.c
+++ s_sources/server.c
@@ -6,7 +6,7 @@
 /*   By: jedusser <jedusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 11:23:37 by jedusser          #+#    #+#             */
-/*   Updated: 2024/04/20 16:23:43 by jedusser         ###   ########.fr       */
+/*   Updated: 2024/04/22 11:16:09 by jedusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,39 +18,66 @@
 
 void    send_signal(int signum, int pid)
 {
-    if(kill(pid, signum) == -1)
-        write(1, "Error\n", 6);
-    
+    if (kill(pid, signum) == -1)
+        write(2, "Error\n", 6);
 }
-char    *init_str(int pid)
+
+void    init_str(char **str, int pid)
 {
-    char *str;
-    
-    str = NULL;
-    str = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
-    if (!str)
-        return (send_signal(SIGUSR2, pid), NULL); //voir gestion d'erreur
+    *str = NULL;
+    *str = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+    if (!*str)
+    {
+        send_signal(SIGUSR2, pid); //voir gestion d'erreur
+        return ; // gestion d'erreur
+    }
     send_signal(SIGUSR1, pid);
-    return (str);
 }
 
+int init_server(struct sigaction *act)
+{
+    act->sa_handler = NULL;
+    act->sa_sigaction = &handler; 
+    act->sa_flags = SA_SIGINFO;
+    sigemptyset(act->sa_mask);
+    if (sigaction(SIGUSR1, act, NULL) || sigaction(SIGUSR2, act, NULL))
+        return (1);
+    return (0);
+}
 
+void    printf_msg(char **str, int pid, size_t *index, size_t *buffer_size)
+{
+    ft_printf("%s\n", *str);
+    send_signal(SIGUSR2, pid);
+    *index = 0;
+    *buffer_size = BUFFER_SIZE;
+    free(*str);
+    *str = NULL;
+    return ;
+}
+void    expand_buffer(char **str, size_t *buffer_size, int pid)
+{
+    char *tmp;
 
-void handler(int signum, siginfo_t *info, void *oldact)
+    tmp = ft_realloc(*str, (*buffer_size + BUFFER_SIZE + 1), ft_strlen(*str));
+    if (!tmp)
+    {
+        free(*str);
+        send_signal(SIGUSR2, pid);
+        return ; //perror gestion
+    }
+    *str = tmp;
+    *buffer_size += BUFFER_SIZE;
+}
+void    handler(int signum, siginfo_t *info, void *oldact)
 {
     static char     *str;
     static short    bit_pos;
     static size_t   index;
     static size_t   buffer_size = BUFFER_SIZE;
-    char            *tmp;
 
     if (!str && info->si_pid > 0)
-    {
-        str = init_str(info->si_pid);
-        if(!str)
-            return ; // crash serveur ? gestion erreur
-        return ; // retour au main
-    } 
+        init_str(&str, info->si_pid);
     if (signum == SIGUSR1)
         str[index] = str[index] <<= 1;
     else if (signum == SIGUSR2)
@@ -62,52 +89,26 @@ void handler(int signum, siginfo_t *info, void *oldact)
     {
         bit_pos = 0;
         if(str[index] == '\0')
-        {
-            ft_printf("%s\n", str);
-            for (int i = 0; i < 5; i++)
-                ft_printf("%d\n", str[i]);
-            send_signal(SIGUSR2, info->si_pid);
-            index = 0;
-            buffer_size = BUFFER_SIZE;
-            free(str);
-            str = NULL;
-            return ;
-        }
+           return (printf_msg(&str, info->si_pid, &index, &buffer_size));
         index++;
         if(index == buffer_size)
-        {
-            tmp = ft_realloc(str, (buffer_size + BUFFER_SIZE + 1), ft_strlen(str));
-            if (!tmp)
-            {
-                free(str);
-                send_signal(SIGUSR2, info->si_pid);
-                return ; //perror gestion
-            }
-            str = tmp;
-            buffer_size += BUFFER_SIZE;
-        }
+           expand_buffer(&str, &buffer_size, info->si_pid);
     }  
     send_signal(SIGUSR1, info->si_pid);
     return ;
 }
 
 
+
 int main(int argc, char **argv)
 {
     struct sigaction act;           
     
-    act.sa_handler = NULL;
-    act.sa_sigaction = &handler; 
-    act.sa_flags = SA_SIGINFO;
-    //sigemptyset(&(act.sa_mask));
-
-    sigaction(SIGUSR1, &act, NULL);
-    sigaction(SIGUSR2, &act, NULL);
+    if(init_server(&act))
+        return (1);
     if (ft_printf("PID Serveur = %d\n", getpid()) == -1)
         return (1);
     while (1)
-    {
         pause();
-    }
     return (0);
 }
